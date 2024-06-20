4383836e63bb67cf742e8fd4d56e5fc906d6ed89
thomas <t.kirsch@webcito.de>
2024-06-20 14:55:27 +0200

    Remove unused isValidCssWidth function and refactor eventListContainer
    The isValidCssWidth function, which was previously used to test whether a given value is a valid CSS width, was removed as it was no longer needed. Additionally, the eventListContainer variable was refactored for cleaner code; its value was directly set to 'list' when the settings.eventListContainer equals null.

--- dist/jquery.bs.calendar.js
+++ dist/jquery.bs.calendar.js
@@ -432,13 +432,6 @@
 
     }
 
-    function isValidCssWidth(value) {
-        // Matches numbers with px, em, rem, vh, vw, % units or no units
-        var numericRegEx = /^([0-9]+(\.[0-9]+)?)(px|em|rem|vh|vw|%)?$/;
-        // Matches keywords such as auto, initial, inherit, unset, fit-content
-        var keywordRegEx = /^(auto|inherit|initial|unset|fit-content)$/;
-        return numericRegEx.test(value) || keywordRegEx.test(value);
-    }
 
     /**
      *
@@ -459,10 +452,10 @@
             `;
         }
         
-        let eventListContainer = '';
+        let eventListContainer;
         let list = '';
         if (settings.eventListContainer === null){
-            eventListContainer = list = `
+            list = `
                  <div class="p-2 js-collapse d-none">
                     <div class="mb-0 rounded-0 border-top" style="width: 100%">
                         <div class="text-center fw-bold py-2 js-day-name bg-transparent"></div>
